80b519b2c5dfd1551bcfd05a60f4c20675153b15
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-05-20 21:50:36 +0000

    Initial version that mostly builds correctly.  Grnuc is still not being built, and some ppl compiles are failing.

---
+++ trunk/Cerad/library/ExtJS/Direct/Base.php
@@ -0,0 +1,46 @@
+<?php
+class ExtJS_Direct_Base
+{
+  protected $context = NULL;
+	
+  static $indexRecords    = 'records';
+  static $indexTotalCount = 'totalCount';
+  static $indexSuccess    = 'success';
+	
+  function __construct($context)
+  {
+    $this->context = $context;
+    $this->init();
+  }
+  protected function init() {}
+  
+  protected function wrapResults($rows = array(),$success = true)
+  {
+    // Not sure why this is here
+    /*
+    switch(count($rows))
+    {
+      case 0:
+        $total = 0;
+  	$records = null;
+  	break;
+      case 1:
+  	$total = 1;
+  	$records = $rows[0];
+  	break;
+      default;
+        $total = count($rows);
+        $records = $rows;
+    }
+     */
+    $total = count($rows);
+    $records = $rows;
+
+    return array
+    (
+      self::$indexSuccess    => $success,
+      self::$indexTotalCount => $total,
+      self::$indexRecords    => $records
+    );
+  }
+}
---
+++ trunk/Cerad/library/ExtJS/FC/Index.php
@@ -0,0 +1,152 @@
+<?php
+/* 
+ * Index Front Controller
+ */
+class ExtJS_FC_Index
+{
+  protected $context = NULL;
+
+  protected $loadFirebug             = FALSE;
+  protected $loadJSFilesIndividually = TRUE;
+
+  protected $jsFiles = array
+  (
+  );
+  function __construct()
+  {
+    $this->init();
+  }
+  protected function init()
+  {
+    $this->setIncludePath();
+  }
+  protected function setIncludePath() {} // Override
+
+  // Spits out one or more js files
+  protected function jsLoad($file)
+  {
+    if ($file) $files = array(str_replace('-','/',$file));
+    else       $files = $this->jsFiles;
+
+    header('Content-Type: text/javascript');
+
+    foreach($files as $file)
+    {
+      require 'JS/' . $file . '.js';
+    }
+  }
+  protected function get(&$post,$name)
+  {
+    if (!isset($post[$name])) return null;
+    $value = $post[$name];
+    unset($post[$name]);
+    return $value;
+  }
+  protected function getContext()
+  {
+    if (!$this->context)
+    {
+      $params = include('Config/' . APP_CONFIG_FILE);
+      $this->context = new Cerad_Context($params);
+    }
+    return $this->context;
+  }
+  protected function direct()
+  {
+    // Most requests are json
+    if(isset($GLOBALS['HTTP_RAW_POST_DATA']))
+    {
+      $requests = json_decode($GLOBALS['HTTP_RAW_POST_DATA'],true);
+    }
+    else
+    {
+      // Form submits use POST, not positive why, maybe file uploads?
+      if (!isset($_POST['extAction'])) exit('Direct POST - No data');
+
+      $post = $_POST;
+
+      $requests = array
+      (
+        'action' => $this->get($post,'extAction'),
+        'method' => $this->get($post,'extMethod'),
+        'type'   => $this->get($post,'extType'),
+        'tid'    => $this->get($post,'extTID'),
+        'upLoad' => $this->get($post,'extUpload'),
+      );
+      // Data is everything left
+      $requests['data'] = array($post);
+    }
+    if (count($requests) < 1) exit('Direct POST - No requests');
+
+    // Always need a context
+    $context = $this->getContext();
+
+    // Might have multiple requests, if so have index array instead of names parameters
+    if (!isset($requests[0])) $requests = array($requests);
+
+    foreach($requests as $data)
+    {
+      // Add a final sanity check here
+
+      // Build class and method name
+      $actionClassName  = 'Direct_' . $data['action'];
+      $actionMethodName = $data['method'];
+
+      $action = new $actionClassName($context);
+
+      // Execute the action
+      $result = $action->$actionMethodName($data['data'][0]);
+
+      $responses[] = array
+      (
+        'type'   => 'rpc',
+        'tid'    => $data['tid'],
+        'action' => $data['action'],
+        'method' => $data['method'],
+        'result' => $result
+      );
+    }
+    // Send it
+    header('Content-Type: text/javascript');
+    echo json_encode($responses);
+  }
+  function execute()
+  {
+    // Directs are always POST but for now just check for a parameter called direct
+    if (isset($_GET['direct'])) return $this->direct();
+
+    // Direct calls are always POST
+    if ($_SERVER['REQUEST_METHOD'] != 'GET')
+    {
+      die("Not direct and not GET, {$_SERVER['REQUEST_METHOD']}");
+    }
+    // Need to determine type of action
+    if (isset($_GET['jsload'])) return $this->jsLoad($_GET['jsload']);
+
+    // Must be an action
+    if (isset($_GET['a'])) $action = $_GET['a'];
+    else                   $action = 'Index';
+
+    $loadFirebug             = $this->loadFirebug;
+    $loadJSFilesIndividually = $this->loadJSFilesIndividually;
+
+    $jsFiles = $this->jsFiles;
+
+    $jsFile  = 'FC/' . $action  . '.js.php';
+    $tplFile = 'FC/' . 'Action.tpl.php';
+
+    // Get the script code
+    ob_start();
+    require $jsFile;
+    $js = ob_get_clean();
+
+    // Template it, not sure if need to buffer it but okay for now
+    ob_start();
+    require $tplFile;
+    $html = ob_get_clean();
+
+    header('Content-Type: text/html');
+    echo $html;
+  }
+}
+?>
