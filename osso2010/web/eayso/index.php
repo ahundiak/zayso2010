6cb42c46378338c1edcf4eef8f2d991961530f40
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-06-30 01:25:21 +0000

    Try agan

--- osso2010/web/eayso/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(E_ALL);
-
-define('APP_CONFIG_WS',   '/home/ahundiak/zayso2010/');
-define('APP_CONFIG_HOST', 'buffy');
-
-require_once APP_CONFIG_WS . 'osso2010/apps/Eayso/FC/Index.php';
-?>
