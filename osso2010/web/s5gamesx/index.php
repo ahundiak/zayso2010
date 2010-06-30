442c1070be59abee11247af2ffdfc692fb7c655a
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-06-30 11:43:33 +0000

    recovery

--- osso2010/web/s5gamesx/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(E_ALL);
-
-define('APP_CONFIG_HOME','/export/home/ahundiak/zayso2010/');
-define('APP_CONFIG_FILE','config.php');
-
-require_once APP_CONFIG_HOME . 'osso2010/apps/S5Games/FC/Index.php';
-?>
