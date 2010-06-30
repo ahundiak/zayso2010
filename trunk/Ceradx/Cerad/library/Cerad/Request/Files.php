70944879aa64c15c2e41d53cefcf133fc82d27cf
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-06-30 11:03:00 +0000

    recovery

--- trunk/Ceradx/Cerad/library/Cerad/Request/Files.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Cerad_Request_Files extends Cerad_Request_Base
-{
-  protected function init()
-  {
-    if (!$this->data) $this->data = $_FILES;
-  }
-}
-?>
--- trunk/Ceradx/Cerad/library/Cerad/Request/Request.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Cerad_Request_Request extends Cerad_Request_Base
-{
-  protected function init()
-  {
-    if (!$this->data) $this->data = $_REQUEST;
-  }
-}
-?>
