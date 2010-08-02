0f591f6a1629aa57c7af09c8641ccfdbf282b1b5
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-02 20:25:26 +0000

    ah

--- trunk/Cerad/library/Cerad/FrontEnd/FrontCont.php
+++ trunk/Cerad/library/Cerad/FrontEnd/FrontCont.php
@@ -1,13 +1,10 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 class Cerad_FrontEnd_FrontCont
 {
   protected $config;
   protected $data;
   protected $contextClassName = 'Cerad_Context';
+  protected $indexFileName    = 'Index.html.php';
 
   protected $loadTypeClassNames = array(
       'css'     => 'Cerad_FrontEnd_LoadCSS',
@@ -15,8 +12,8 @@ class Cerad_FrontEnd_FrontCont
       'direct'  => 'Cerad_FrontEnd_LoadDirect',
 
       'action'  => 'FrontEnd_LoadAction',
-      
       'html'    => 'FrontEnd_LoadHTML',
+      'tab'     => 'FrontEnd_LoadTab',
   );
   public function __construct($config)
   {
@@ -54,7 +51,51 @@ class Cerad_FrontEnd_FrontCont
     $this->data['context'] = $context;
     return $context;
   }
-  function execute()
+  // The args can eith be 'action/controller' or an array
+  function execute($args = null)
+  {
+    // Use pretty urls, pull args from the uri
+    if (!$args)
+    {
+      $uri  = $_SERVER['REQUEST_URI'];
+      $name = $_SERVER['SCRIPT_NAME'];
+
+      if (substr($uri,0,strlen($name)) == $name) $args = substr($uri,strlen($name)+1);
+      else                                       $args = substr($uri,strlen(dirname($name)) + 1);
+
+      // Need to pull out any thing after ?
+      $tmp  = explode('?',$args);
+      $args = $tmp[0];
+
+      // echo "URI: $uri <br />NAME: $name<br />ARGS: $args</br />";
+      // if (!$args) $args = 'index';
+    }
+    // Convert to array
+    if (!is_array($args)) $args = explode('/',$args);
+
+    // Always get at least one element, might be ''
+    if ($args[0] == '')
+    {
+      // Load in the default file
+      header('Content-type: text/html');
+      include  $this->indexFileName;
+      return;
+    }
+
+    $loadType = $args[0];
+    if (!isset($this->loadTypeClassNames[$loadType])) $loadType = 'action'; // default
+    else                                              array_shift($args);   // pop the load type
+
+    $loadTypeClassName = $this->loadTypeClassNames[$loadType];
+
+    // For action always make sure have an argument
+    if (($loadType == 'action') && (count($args) < 1)) $args = array('index');
+
+    // And execute it
+    $load = new $loadTypeClassName($this->context);
+    $load->execute($args);
+  }
+  function executeFromRequest()
   {
     $context = $this->context;
     $request = $context->request;
