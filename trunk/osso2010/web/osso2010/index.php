6408da7197354b800830a8e86d2a777ba19cbce1
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-02 20:29:21 +0000

    ah

--- trunk/osso2010/web/osso2010/index.php
+++ trunk/osso2010/web/osso2010/index.php
@@ -1,5 +1,11 @@
 <?php
+
+$log = $_SERVER['REQUEST_URI'] . "\n";
+file_put_contents('log.txt',$log,FILE_APPEND);
+
 $config = include '../config.php';
+
+$config['web_app_path'] = '/osso2010/';
 
 include $config['ws'] . 'osso2010/apps/osso/FrontEnd/FrontCont.php';
 
