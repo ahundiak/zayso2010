fcca26ae32393cb0628a06ef0ff72182fe43061d
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-09 17:13:42 +0000

    ah

--- osso2010/web/osso2010/index.php
+++ osso2010/web/osso2010/index.php
@@ -1,6 +1,9 @@
 <?php
 
-$log = $_SERVER['REQUEST_URI'] . "\n";
+if (isset($_COOKIE['osso2010'])) $sessionId = $_COOKIE['osso2010'];
+else                             $sessionId = 'NO SESSION ID';
+
+$log = $_SERVER['REQUEST_URI'] . ' ' . $sessionId . "\n";
 file_put_contents('log.txt',$log,FILE_APPEND);
 
 $config = include '../config.php';
