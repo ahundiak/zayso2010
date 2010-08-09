76d90f685861f7c5719e886aa83f752f077b2b29
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-09 14:10:25 +0000

    ah

---
+++ osso2010/model/config/config_zayso.php
@@ -0,0 +1,49 @@
+<?php
+// Want to add some code to detect host to allow these to be run on web machines
+// or other development machines
+return array
+(
+  'ws'    => '/home/ahundiak/zayso2010/',
+  'datax' => '/home/ahundiak/datax/',
+
+  'dbParams' => array
+  (
+    'default' => array (
+      'host'     => 'db.telena.com',
+      'username' => 'impd',
+      'password' => 'impd894',
+      'dbname'   => 'xxx',
+      'dbtype'   => 'mysql',
+      'adapter'  => 'pdo_mysql'
+    ),
+    'dbMain' => array (
+      'host'     => '127.0.0.1',
+      'username' => 'impd',
+      'password' => 'impd894',
+      'dbname'   => 'osso2007',
+      'dbtype'   => 'mysql',
+      'adapter'  => 'pdo_mysql'
+    ),
+    'dbOsso2007' => array (
+      'dbname'   => 'osso2007',
+    ),
+    'dbOsso' => array (
+      'dbname'   => 'osso',
+    ),
+    'dbEayso' => array (
+      'dbname'   => 'eayso',
+    ),
+    'dbSession' => array (
+      'dbname'   => 'session',
+    ),
+    'dbS5Games' => array (
+      'host'     => '127.0.0.1',
+      'username' => 'impd',
+      'password' => 'impd894',
+      'dbname'   => 's5games',
+      'dbtype'   => 'mysql',
+      'adapter'  => 'pdo_mysql'
+    ),
+  ),
+);
+?>
