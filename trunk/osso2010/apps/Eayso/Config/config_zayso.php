b97015557f06661e6e37661876926499e5d446e9
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-12 19:34:36 +0000

    ah

--- trunk/osso2010/apps/Eayso/Config/config_zayso.php
+++ trunk/osso2010/apps/Eayso/Config/config_zayso.php
@@ -1,21 +1,46 @@
 <?php
+// Want to add some code to detect host to allow these to be run on web machines
+// or other development machines
 return array
 (
-  'dbs' => array
+  'ws'    => '/home/ahundiak/zayso2010/',
+  'datax' => '/home/ahundiak/datax/',
+
+  'dbParams' => array
   (
-    'dbMain' => array (
-      'host'     => 'db.telavant.com',
+    'default' => array (
+      'host'     => 'db.televant.com',
       'username' => 'impd',
       'password' => 'impd894',
-      'dbname'   => 'eayso',
+      'dbname'   => 'xxx',
       'dbtype'   => 'mysql',
       'adapter'  => 'pdo_mysql'
     ),
-    'dbEayso' => array (
-      'host'     => 'db.telavant.com',
+    'dbMain' => array (
+      'host'     => 'db.televant.com',
       'username' => 'impd',
       'password' => 'impd894',
+      'dbname'   => 'osso2007',
+      'dbtype'   => 'mysql',
+      'adapter'  => 'pdo_mysql'
+    ),
+    'dbOsso2007' => array (
+      'dbname'   => 'osso2007',
+    ),
+    'dbOsso' => array (
+      'dbname'   => 'osso',
+    ),
+    'dbEayso' => array (
       'dbname'   => 'eayso',
+    ),
+    'dbSession' => array (
+      'dbname'   => 'session',
+    ),
+    'dbS5Games' => array (
+      'host'     => '127.0.0.1',
+      'username' => 'impd',
+      'password' => 'impd894',
+      'dbname'   => 's5games',
       'dbtype'   => 'mysql',
       'adapter'  => 'pdo_mysql'
     ),
