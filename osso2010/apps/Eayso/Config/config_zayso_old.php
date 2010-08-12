40ba84dd0b1619c354317e98b6b0a2d8fce2c3a5
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-12 19:34:36 +0000

    ah

---
+++ osso2010/apps/Eayso/Config/config_zayso_old.php
@@ -0,0 +1,24 @@
+<?php
+return array
+(
+  'dbs' => array
+  (
+    'dbMain' => array (
+      'host'     => 'db.telavant.com',
+      'username' => 'impd',
+      'password' => 'impd894',
+      'dbname'   => 'eayso',
+      'dbtype'   => 'mysql',
+      'adapter'  => 'pdo_mysql'
+    ),
+    'dbEayso' => array (
+      'host'     => 'db.telavant.com',
+      'username' => 'impd',
+      'password' => 'impd894',
+      'dbname'   => 'eayso',
+      'dbtype'   => 'mysql',
+      'adapter'  => 'pdo_mysql'
+    ),
+  ),
+);
+?>
