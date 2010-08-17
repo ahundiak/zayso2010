75408120637642e2fd770769b2115d8aa8e3df55
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-17 15:12:16 +0000

    ah

---
+++ osso2010/web/openid/index.php
@@ -0,0 +1,25 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<html>
+  <head>
+  </head>
+  <body>
+    <a class="rpxnow" onclick="return false;"
+      href="https://zayso.rpxnow.com/openid/v2/signin?token_url=http%3A%2F%2Flocal.osso2010.org/openid%2Frpx.php"> Sign In </a>
+    
+    <script type="text/javascript">
+      var rpxJsHost = (("https:" == document.location.protocol) ? "https://" : "http://static.");
+      document.write(unescape("%3Cscript src='" + rpxJsHost +
+        "rpxnow.com/js/lib/rpx.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+  RPXNOW.overlay = true;
+  RPXNOW.language_preference = 'en';
+</script>
+  </body>
+</html>
