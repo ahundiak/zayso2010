092377c76b0694c953b3951337e4d909d8768914
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-18 19:32:20 +0000

    ah

--- osso2007/osso2007/apps/osso/library/mvc/Person/PersonListView.php
+++ osso2007/osso2007/apps/osso/library/mvc/Person/PersonListView.php
@@ -32,7 +32,19 @@ class PersonListView extends Proj_View
             $personItems = $models->PersonVolModel->search($data);
         }
         $this->personItems = $personItems;
+
+        // Hack in the eayso stuff
+        $personIds = array();
+        foreach($this->personItems as $item)
+        {
+          $personIds[$item->id] = $item->id;
+        }
+        $directPerson = new Osso2007_Person_PersonDirect($this->context);
+        $result = $directPerson->getCerts(array('person_id' => $personIds));
+        $this->certs = $result->rows;
         
+        $this->repoCert = new Eayso_Reg_Cert_RegCertRepo($this->context);
+
         /* Render it */        
         return $this->renderx();
     }
@@ -41,28 +53,56 @@ class PersonListView extends Proj_View
         // $name = $person->lastName . ', ' . $person->firstName;
         return $this->escape($person->namex);
     }
+    function displayCerts($person)
+    {
+      $id = $person->id;
+      if (!isset($this->certs[$id])) return 'Not in eayso';
+      $item = $this->certs[$id];
+
+      $lines = array();
+
+      $fname = $item['eayso_fname'];
+      $nname = $item['eayso_nname'];
+      $lname = $item['eayso_lname'];
+
+      if ($nname) $name = $fname . ' (' . $nname . ') ' . $lname;
+      else        $name = $fname . ' ' .$lname;
+
+      $lines[] = $this->escape($name);
+      
+      $line = 'MY' . $item['eayso_reg_year'] . ' ' . $item['eayso_aysoid'];
+      $lines[] = $line;
+
+      foreach($item['certs'] as $cert)
+      {
+        $line = $this->repoCert->getDesc($cert['cert_type']);
+        $lines[] = $this->escape($line);
+      }
+
+      return implode("<br \>\n",$lines);
+    }
     function displayVolList($vols)
     {
-        $db = $this->context->db;
+        // $db = $this->context->db;
         $cnt = 0;
         $max = 6;
         $html = NULL;
         $vols = array_values($vols);
         for($i = count($vols) - 1; $i >= 0; $i--)
         {
-          $vol = $vols[$i];
-          $cnt++;
+          $vol    = $vols[$i];
           $unit   = $vol->unitKey;        //UnitModel::getKey       ($db,$vol->unitId);
           $year   = $vol->year;           //YearModel::getDesc      ($db,$vol->regYearId);
           $season = $vol->seasonTypeDesc; // SeasonTypeModel::getDesc($db,$vol->seasonTypeId);
           $div    = $vol->divisionDesc;   // DivisionModel::getDesc  ($db,$vol->divisionId);
           $job    = $vol->volTypeDesc;    // VolTypeModel::getDesc   ($db,$vol->volTypeId);
 
-          if ($cnt <= $max)
+          if ($cnt <= $max && $vol->volTypeId != 27) // Zayso admin
           {
             if ($html) $html .= "<br />\n";
             $html .= "{$unit} {$year} {$season} {$job}";
             if ($div) $html .= " {$div}";
+            $cnt++;
           }
         }
         return $html;
