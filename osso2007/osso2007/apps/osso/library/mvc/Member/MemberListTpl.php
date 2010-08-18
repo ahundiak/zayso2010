389bdeb1e6ce9d3982c62b1c6c80142d1a462a2d
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-18 18:38:32 +0000

    ah

--- osso2007/osso2007/apps/osso/library/mvc/Member/MemberListTpl.php
+++ osso2007/osso2007/apps/osso/library/mvc/Member/MemberListTpl.php
@@ -1,23 +1,64 @@
 <table border="1">
-<tr><th colspan="7">List Account Members</th></tr>
+<tr><th colspan="5">List Account Members</th></tr>
 <tr>
     <td>Edit</td>
     <td>Region</td>
-    <td>Account Name</td>
     <td>Member Name</td>
     <td>eAYSO Information</td>
-    <td>Status</td>
     <td>Level</td>
 </tr>
-<?php foreach($this->members as $member) { ?>
+<?php
+
+  $repoCert = new Eayso_Reg_Cert_RegCertRepo($this->context);
+  
+  foreach($this->membersx as $item)
+  {
+    $memberId       = $item['member_id'];
+    $memberUnitDesc = $item['member_unit_desc'];
+    $accountName    = $item['account_name'];
+
+    $memberName     = $item['member_name']  . ' ' . $item['account_name'];
+    $personName     = $item['person_fname'] . ' ' . $item['person_lname'];
+
+    if ($item['member_level'] == 1) $memberLevel = 'Primary';
+    else                            $memberLevel = 'Secondary';
+
+    if ($item['eayso_aysoid'])
+    {
+      $lines = array();
+
+      $fname = $item['eayso_fname'];
+      $nname = $item['eayso_nname'];
+      $lname = $item['eayso_lname'];
+
+      if ($nname) $name = $fname . ' (' . $name . ')' . $lname;
+      else        $name = $fname . ' ' .$lname;
+
+      // $lines[] = $this->escape($name);
+
+      $line = $name . ' ' . $item['eayso_aysoid'] . ' MY' . $item['eayso_reg_year'];
+      $lines[] = $this->escape($line);
+
+      foreach($item['certs'] as $cert)
+      {
+        $line = $repoCert->getDesc($cert['cert_type']);
+        $lines[] = $this->escape($line);
+      }
+   }
+    else {
+      $lines = array();
+      $lines[] = $this->escape($personName);
+      $lines[] = 'Not linked to eayso';
+    }
+    $eaysoInfo = implode("<br />\n",$lines);
+
+?>
 <tr>
-    <td><?php echo $this->href('Edit','member_edit',$member->id); ?></td>
-    <td><?php echo $this->escape($member->unitDesc);   ?></td>
-    <td><?php echo $this->escape($member->account->name); ?></td>
-    <td><?php echo $this->escape($member->name);       ?></td>
-    <td><?php echo $this->escape($member->personName); ?></td>
-    <td><?php echo $this->escape($member->status);     ?></td>
-    <td><?php echo $this->escape($member->level);      ?></td>
+    <td><?php echo $this->href('Edit','member_edit',$memberId); ?></td>
+    <td><?php echo $this->escape($memberUnitDesc);   ?></td>
+    <td><?php echo $this->escape($memberName);       ?></td>
+    <td><?php echo               $eaysoInfo;         ?></td>
+    <td><?php echo $this->escape($memberLevel);      ?></td>
 </tr>
 <?php } ?>
 <tr>
