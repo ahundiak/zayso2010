b32b7aaaa17ad017f6742f285c1ca92ee138577c
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-22 16:56:34 +0000

    ah

--- trunk/Cerad/library/Cerad/Session/Base.php
+++ trunk/Cerad/library/Cerad/Session/Base.php
@@ -1,10 +1,12 @@
 <?php
 class Cerad_Session_Base
 {
+  protected $data = NULL;
   protected $context = NULL;
   protected $sessionCookieName = 'cerad';
+  protected $sessionCookiePath = '/';
   protected $sessionCookieLifetime = 0;
-  
+
   public function __construct($context)
   {
     $this->context = $context;
@@ -18,8 +20,12 @@ class Cerad_Session_Base
 
   public function set($name,$item) {}
 
-  public function save() {}
+  public function   __get($name) { return $this->get($name); }
+  public function __isset($name) { return $this->has($name); }
+  public function   __set($name,$value) { return $this->set($name,$value); }
 
+  public function save()    {}
+  public function destroy() {}
   public function newSessionData() { return $this->context->getSessionData(); }
 }
 ?>
