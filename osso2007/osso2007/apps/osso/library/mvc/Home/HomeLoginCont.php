87984ad8e5745d5084bfcd85ee82bc9d3b975ff1
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-22 17:33:49 +0000

    ah

--- osso2007/osso2007/apps/osso/library/mvc/Home/HomeLoginCont.php
+++ osso2007/osso2007/apps/osso/library/mvc/Home/HomeLoginCont.php
@@ -14,50 +14,51 @@ class HomeLoginCont extends Proj_Controller_Action
         
         return;
     }
-    public function processActionPost()
+  public function processActionPost()
+  {
+    $request  = $this->getRequest();
+    $response = $this->getResponse();
+    $models   = $this->context->models;
+        
+    $accountUser = $request->getPost('login_account_user');
+    $accountPass = $request->getPost('login_account_pass');
+    $memberName  = $request->getPost('login_member_name');
+
+    if (!$accountUser)
     {
-        $request  = $this->getRequest();
-        $response = $this->getResponse();
-        $models   = $this->context->models;
+      $response->setRedirect($this->link('home_login'));
+      return;
+    }        
+    $accountSearch = new SearchData();
+    $accountSearch->user = $accountUser;
         
-        $accountUser = $request->getPost('login_account_user');
-        $accountPass = $request->getPost('login_account_pass');
-        $memberName  = $request->getPost('login_member_name');
-
-        if (!$accountUser) {
-            $response->setRedirect($this->link('home_login'));
-            return;            
-        }        
-        $accountSearch = new SearchData();
-        $accountSearch->user = $accountUser;
+    $account = $models->AccountModel->searchOne($accountSearch);
         
-        $account = $models->AccountModel->searchOne($accountSearch);
+    if (!$account || (($accountPass != 'soccer894') && ($account->pass != md5($accountPass))))
+    {
+      $response->setRedirect($this->link('home_login'));
+      return;            
+    }
+    $memberSearch = new SearchData();
+    $memberSearch->accountId = $account->id;
+    if ($memberName) $memberSearch->name  = $memberName;
+    else             $memberSearch->level = 1;
         
-        if (!$account || (($accountPass != 'soccer894') && ($account->pass != md5($accountPass)))) {
-            $response->setRedirect($this->link('home_login'));
-            return;            
-        }        
-        $memberSearch = new SearchData();
-        $memberSearch->accountId = $account->id;
-        if ($memberName) $memberSearch->name  = $memberName;
-        else             $memberSearch->level = 1;
+    $member = $models->MemberModel->searchOne($memberSearch);
         
-        $member = $models->MemberModel->searchOne($memberSearch);
+    if (!$member)
+    {
+      $response->setRedirect($this->link('home_login'));
+      return;            
+    }        
+    $defaults = $this->context->config['user']; // ->toArray();
         
-        if (!$member) {
-            $response->setRedirect($this->link('home_login'));
-            return;            
-        }        
-
-        $defaults = $this->context->config->user->toArray();
-        
-        // if ($defaults['unit_id'] != 4) $defaults['season_type_id'] = 3;
-        
-        $user = $models->UserModel->load($defaults,$member->id);
+    // if ($defaults['unit_id'] != 4) $defaults['season_type_id'] = 3;    
+    $user = $models->UserModel->load($defaults,$member->id);
       
-        $this->context->session->user = $user;
-        
-        $response->setRedirect($this->link('account_index'));   
-    } 
+    $this->context->session->user = $user;
+    
+    $response->setRedirect($this->link('account_index'));   
+  } 
 }
 ?>
