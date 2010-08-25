9e6800ef5c39c5310f78abeb2e66cd9af7631126
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-08-25 16:41:24 +0000

    ah

--- trunk/osso2010/web/arbiter/index.php
+++ trunk/osso2010/web/arbiter/index.php
@@ -1,39 +1,13 @@
 <?php
-  $extDir = '../tools/ext/';
-  $title  = 'Arbiter Ref Availability Report';
+error_reporting(E_ALL);
+$config = array
+(
+  'ws'        => '/home/ahundiak/zayso2010/',
+  'db_host'   => '127.0.0.1',
+  'web_host'  => 'buffy',
+  'web_path'  => '/arbiter/',
+  'web_tools' => '/tools/',
+);
+require_once $config['ws'] . 'osso2010/apps/Arbiter/FrontEnd/FrontCont.php';
+exit();
 ?>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-    <title><?php echo $title; ?></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo $extDir; ?>resources/css/ext-all.css" />
-    <script type="text/javascript" src="<?php echo $extDir; ?>adapter/ext/ext-base.js"> </script>
-    <script type="text/javascript" src="<?php echo $extDir; ?>ext-all-debug.js"> </script>
-
-    <link rel="stylesheet" type="text/css" href="<?php echo $extDir; ?>ux/fileuploadfield/css/fileuploadfield.css"/>
-    <script type="text/javascript" src="<?php echo $extDir; ?>ux/fileuploadfield/FileUploadField.js"></script>
-    
-    <!--
-    <script type="text/javascript" src="VolGrid.js"></script>
-    -->
-    <script type="text/javascript" src="ref-avail.js"></script>
-
-    <link rel="stylesheet" type="text/css" href="<?php echo $extDir; ?>shared/examples.css" />
-    <style type="text/css">
-        .upload-icon {
-            background: url('<?php echo $extDir; ?>shared/icons/fam/image_add.png') no-repeat 0 0 !important;
-        }
-        #fi-button-msg {
-            border: 2px solid #ccc;
-            padding: 5px 10px;
-            background: #eee;
-            margin: 5px;
-            float: left;
-        }
-    </style>
-</head>
-<body>
-    <h1>Upload Referee Availability Report</h1>
-    <div id="fi-form"></div>
-</body>
-</html>
