a789c637ec8a4b0b7b1577cff742e8da735e12be
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-09-04 23:56:41 +0000

    ah

--- osso2010/web/osso2007/index.php
+++ osso2010/web/osso2007/index.php
@@ -3,44 +3,10 @@ error_reporting(E_ALL);
 $config = array
 (
   'ws'        => '/home/ahundiak/zayso2010/',
-  'db_host'   => '127.0.0.1',
   'web_host'  => 'buffy',
+  'web_path'  => '/osso2007/',
   'web_tools' => '/tools/',
 );
-require_once $config['ws'] . 'osso2007/osso2007/apps/osso/FrontEnd/FrontContx.php';
+require_once $config['ws'] . 'osso2010/apps/Osso2007/FrontEnd/FrontCont.php';
 exit();
-
-error_reporting(0);
-
-/* Wrap everything in a class to avoid global variables */
-class WebIndex
-{
-	static function run()
-	{
-		/* This could get moved to .htaccess */
-		ini_set('include_path','.' . 
-			PATH_SEPARATOR . '/home/ahundiak/zayso2010/osso2007/osso2007/library' .
-			PATH_SEPARATOR . '/home/ahundiak/zayso2010/osso2007/osso2007/data' .
-			PATH_SEPARATOR . '/home/ahundiak/zayso2010/osso2007/osso2007/data/classes' .
-            		PATH_SEPARATOR . '/home/ahundiak/zayso2010/osso2007/osso2007/apps/osso/library' .
-            		PATH_SEPARATOR . '/home/ahundiak/zayso2010/ZendFramework-1.0.0/library' .
-            		PATH_SEPARATOR . '/home/ahundiak/zayso2010/Cerad/library' . 
-            		PATH_SEPARATOR . '/home/ahundiak/zayso2010/osso2010/model/classes'
-		);
-		//echo phpinfo(); die();
-
-		$params['proj_dir'] = '/home/ahundiak/zayso2010/osso2007/osso2007';
-		$params['app_dir']  = '/home/ahundiak/zayso2010/osso2007/osso2007/apps/osso';
-		
-		$params['config_file_name'] = 'buffy2007.ini';
-		
-		/* Loadin the Project and Application Context */
-		require_once $params['proj_dir'] . '/config/ProjectContext.php';
-		require_once $params['app_dir']  . '/config/ApplicationContext.php';
-		$context = new ApplicationContext($params);		
-		$context->fc->dispatch();
-	}
-}
-WebIndex::run();
-
 ?>
