074b0c0e48eba6ffb10884e885d06305cf787b70
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-09-07 00:56:24 +0000

    ah

--- osso2010/web/osso2007/index.php
+++ osso2010/web/osso2007/index.php
@@ -3,10 +3,8 @@ error_reporting(E_ALL);
 $config = array
 (
   'ws'        => '/home/ahundiak/zayso2010/',
-  'web_name'  => 'local.osso2010.org', // No longer used
   'web_host'  => 'buffy',
-  'web_path'  => '/osso2007/',         // No longer used
-  'web_tools' => 'tools',              // Not being used
+  'web_tools' => 'tools',  // Not being used but probably should be
 );
 require_once $config['ws'] . 'osso2010/apps/Osso2007/FrontEnd/FrontCont.php';
 exit();
