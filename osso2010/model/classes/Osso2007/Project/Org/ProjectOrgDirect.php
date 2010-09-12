c5c40fd54cbddeb94a409d6e6661340a3d19bce9
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-09-12 13:33:31 +0000

    ah

--- osso2010/model/classes/Osso2007/Project/Org/ProjectOrgDirect.php
+++ osso2010/model/classes/Osso2007/Project/Org/ProjectOrgDirect.php
@@ -4,5 +4,6 @@ class Osso2007_Project_Org_ProjectOrgDirect extends Cerad_Direct_Base
   protected $tblName = 'osso2007.project_org';
   protected $idName  = 'id';
   protected $dbName  = 'dbOsso2007';
+  protected $ignoreDupKey = true;
 }
 ?>
