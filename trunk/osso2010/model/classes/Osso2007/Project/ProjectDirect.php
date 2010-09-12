7c9291344f3cde62da9ff634a20628660be5bdba
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-09-12 13:38:02 +0000

    ah

--- trunk/osso2010/model/classes/Osso2007/Project/ProjectDirect.php
+++ trunk/osso2010/model/classes/Osso2007/Project/ProjectDirect.php
@@ -4,5 +4,6 @@ class Osso2007_Project_ProjectDirect extends Cerad_Direct_Base
   protected $tblName = 'osso2007.project';
   protected $idName  = 'id';
   protected $dbName  = 'dbOsso2007';
+  protected $ignoreDupKey = true;
 }
 ?>
