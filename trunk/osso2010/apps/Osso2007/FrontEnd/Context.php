766fc0f9bb49534ce679c8e1e1d448ad8ef33053
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-09-17 14:53:09 +0000

    ah

--- trunk/osso2010/apps/Osso2007/FrontEnd/Context.php
+++ trunk/osso2010/apps/Osso2007/FrontEnd/Context.php
@@ -1,5 +1,5 @@
 <?php
-class Osso2007_Context extends Cerad_Context
+class Osso2007_FrontEnd_Context extends Cerad_Context
 {
   protected $classNames = array
   (
@@ -11,6 +11,9 @@ class Osso2007_Context extends Cerad_Context
 
     'url'      => 'Osso2007_Url',
     'html'     => 'Cerad_HTML',
+
+    'repos'    => 'Osso2007_Repos',
+    'tables'   => 'Osso2007_Tables',
   );
 }
 
