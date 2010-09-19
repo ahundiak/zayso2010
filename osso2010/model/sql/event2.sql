dd9fbe07ffb4c030d4532af453b5f8468d69ad09
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-09-19 13:54:14 +0000

    ah

--- osso2010/model/sql/event2.sql
+++ osso2010/model/sql/event2.sql
@@ -23,12 +23,15 @@ CREATE TABLE         `event_class`
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 INSERT INTO event_class (id,key1,desc1) VALUES
-(1,'RG','RG - Regular Game' ),
+(1,'RG','RG - Regular Game'),
 (2,'PP','PP - Pool Play'),
 (3,'QF','QF - Quarter Final'),
 (4,'SF','SF - Semi Final'),
-(5,'F', 'F  - Final');
+(5,'F', 'F  - Final'),
+(6,'CM','CM - Consolation Match');
 
 ALTER TABLE event
-ADD COLUMN class_id int(10) unsigned default 0
+ADD COLUMN class_id int(10) unsigned default 1
 AFTER event_type_id;
+
+UPDATE event SET class_id = 1;
