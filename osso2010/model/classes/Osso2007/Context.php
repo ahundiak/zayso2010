fb7f285a269a64115bc37bb8a2a53b03c96eb601
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2010-10-17 15:51:23 +0000

    ah

--- osso2010/model/classes/Osso2007/Context.php
+++ osso2010/model/classes/Osso2007/Context.php
@@ -3,8 +3,8 @@ class Osso2007_Context extends Cerad_Context
 {
   protected function init()
   {
-    $this->classNames['repos']  = 'Osso2007_Repos';
-    $this->classNames['tables'] = 'Osso2007_Tables';
+    $this->classNames['repos']   = 'Osso2007_Repos';
+    $this->classNames['tablesx'] = 'Osso2007_Tables';
   }
 }
 
