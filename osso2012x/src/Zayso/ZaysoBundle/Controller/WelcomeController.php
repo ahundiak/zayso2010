3c9223a3ef3002f20258a435b898fafc29d402e7
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2011-08-09 00:43:24 +0000

    ah

--- osso2012x/src/Zayso/ZaysoBundle/Controller/WelcomeController.php
+++ osso2012x/src/Zayso/ZaysoBundle/Controller/WelcomeController.php
@@ -16,6 +16,28 @@ class WelcomeController extends Controller
     (
         'base' => $base,
     );
+    
+    $statusList=array(
+	'admin'    => 'Administrator',
+	'operator' => 'Operator',
+	'agent'    => 'Marketing Agent'
+    );
+
+    if (true) {
+	$data['h1Text']='Pat Benatar';
+	$data['status']='admin';
+	$data['h1Class']='agentname';
+        $data['h2Text'] = 'Administrator';
+    } else {
+	$data['h1Text']='Login Required.';
+	$data['h1Class']='';
+	$data['status']='';
+        $data['h2Text'] = '';
+    }
+    $data['dater'] = date('r');
+    
+    $data['data'] = $data;
+    
     return $this->render('ZaysoBundle:Welcome:welcome.html.twig',$data);
   }
 }
