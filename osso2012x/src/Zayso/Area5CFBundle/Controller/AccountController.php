d4ea0a3591bc8937d398dd6863290a2e8b9a3092
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2011-08-11 20:14:14 +0000

    ah

---
+++ osso2012x/src/Zayso/Area5CFBundle/Controller/AccountController.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Zayso\Area5CFBundle\Controller;
+
+class AccountController extends BaseController
+{
+    public function signoutAction()
+    {
+        $session = $this->getSession();
+        $session->set('userData',null);
+        return $this->redirect($this->generateUrl('_area5cf_welcomex'));
+    }
+    public function signinAction()
+    {
+        $session = $this->getSession();
+        $accountSigninData = $session->get('accountSigninData');
+
+        if ($accountSigninData)
+        {
+            $errors = $session->getFlash('errors');
+            $accountSigninData['errors'] = $errors;
+        }
+        else
+        {
+            $accountSigninData = array
+            (
+                'userName' => '',
+                'userPass' => '',
+                'errors'   => array(),
+            );
+        }
+        $tplData = $this->getTplData();
+        $tplData['accountSigninData'] = $accountSigninData;
+        
+        return $this->render('Area5CFBundle:Account:signin.html.twig',$tplData);
+    }
+    public function signinPostAction()
+    {
+        $em = $this->getEntityManager();
+
+        $request = $this->getRequest();
+        $session = $this->getSession();
+
+        $accountSigninData = $request->request->get('accountSigninData');
+
+        // Really don't want to store the passwords
+        $accountSigninDatax = $accountSigninData;
+        $accountSigninDatax['userPass'] = '';
+        $session->set('accountSigninData',$accountSigninDatax);
+
+        $accountRepo = $em->getRepository('ZaysoBundle:Account');
+        $account = $accountRepo->findForUserName($accountSigninData['userName'],$accountSigninData['userPass']);
+        if (is_array($account))
+        {
+            $session->setFlash('errors',$account);
+            return $this->redirect($this->generateUrl('_area5c_account_signin'));
+        }
+        // Signin
+        $member = $account->getPrimaryMember();
+        $userData = array
+        (
+            'accountId' => $account->getId(),
+            'memberId'  => $member->getId(),
+            'personId'  => $member->getPerson()->getId(), // Probably don't need
+            'projectId' => $this->getProjectId(),
+        );
+        $session->set('userData',$userData);
+
+        return $this->redirect($this->generateUrl('_area5cf_home'));
+    }
+    public function createAction()
+    {
+        return $this->redirect($this->generateUrl('_area5cf_welcomex'));
+        $tplData = $this->getTplData();
+    
+        $session = $this->getSession();
+        $accountCreateData = $session->get('accountCreateData');
+    
+        if ($accountCreateData)
+        {
+            $errors = $session->getFlash('errors');
+            $accountCreateData['errors'] = $errors;
+        }
+        else
+        {
+            $accountCreateData = array
+            (
+                'userName'  => '',
+                'userPass1' => '',
+                'userPass2' => '',
+                'aysoid'    => '',
+                'firstName' => '',
+                'lastName'  => '',
+                'nickName'  => '',
+                'email'     => '',
+                'cellPhone' => '',
+                'region'    => 0,
+                'refBadge'  => 'None',
+                'errors'    => null, // array('E1','E2'),
+            );
+        }
+        $refBadgePickList = array
+        (
+            'None'         => 'None',
+            'Regional'     => 'Regional',
+            'Intermediate' => 'Intermediate',
+            'Advanced'     => 'Advanced',
+            'National'     => "National",
+            'National 2'   => 'National 2',
+            'Assistant'    => 'Assistant',
+            'U8 Official'  => 'U8',
+        );
+        $tplData['accountCreateData'] = $accountCreateData;
+        $tplData['refBadgePickList']  = $refBadgePickList;
+    
+        return $this->render('NatGamesBundle:Account:create.html.twig',$tplData);
+    }
+    public function createPostAction()
+    {
+        return $this->redirect($this->generateUrl('_area5cf_welcomex'));
+        $em = $this->getEntityManager();
+
+        $request = $this->getRequest();
+    
+        $accountCreateData = $request->request->get('accountCreateData');
+
+        // Really don't want to store the passwords
+        $accountCreateDatax = $accountCreateData;
+        $accountCreateDatax['userPass1'] = '';
+        $accountCreateDatax['userPass2'] = '';
+        $session = $this->getRequest()->getSession();
+        $session->set('accountCreateData',$accountCreateDatax);
+
+        $accountRepo = $em->getRepository('ZaysoBundle:Account');
+        $account = $accountRepo->create($accountCreateData);
+        if (is_array($account))
+        {
+            $session->setFlash('errors',$account);
+            return $this->redirect($this->generateUrl('_natgames_account_create'));
+        }
+
+        // Setup project person
+        $projectId = $this->getProjectId();
+        $personId  = $account->getPrimaryPersonId();
+
+        $projectRepo = $em->getRepository('ZaysoBundle:Project');
+
+        $projectPerson = $projectRepo->loadProjectPerson($projectId,$personId);
+        $projectPerson->set('accountCreateData',$accountCreateDatax);
+
+        $todo = array('projectPlans' => true, 'openid' => true, 'projectLevels' => true);
+        $projectPerson->set('todo',$todo);
+        $em->flush();
+
+        // Signin
+        $member = $account->getPrimaryMember();
+        $userData = array
+        (
+            'accountId' => $account->getId(),
+            'memberId'  => $member->getId(),
+            'personId'  => $personId,
+            'projectId' => $projectId,
+        );
+        $session->set('userData',$userData);
+
+        // Also save signin information
+        $accountSigninData = array
+        (
+            'userName' => $account->getUserName(),
+            'userPass' => '',
+        );
+        $session->set('accountSigninData',$accountSigninData);
+        //
+        //print_r($accountCreateData); die();
+    
+        return $this->redirect($this->generateUrl('_natgames_home'));
+  }
+}
--- osso2012x/src/Zayso/Area5CFBundle/Controller/WelcomeController.php
+++ osso2012x/src/Zayso/Area5CFBundle/Controller/WelcomeController.php
@@ -12,32 +12,9 @@ class WelcomeController extends BaseController
     public function homeAction()
     {
         $user = $this->getUser();
-        if (!$user->isSignedIn()) return $this->redirect($this->generateUrl('_natgames_welcomex'));
+        if (!$user->isSignedIn()) return $this->redirect($this->generateUrl('_area5cf_welcomex'));
 
-        $todo = $user->getProjectPerson()->get('todo');
-        if (isset($todo['projectPlans']) && $todo['projectPlans'])
-        {
-            $todo['projectPlans'] = false;
-            $user->getProjectPerson()->set('todo',$todo);
-            $this->getEntityManager()->flush();
-            return $this->redirect($this->generateUrl('_natgames_project_plans'));
-        }
-        if (isset($todo['projectLevels']) && $todo['projectLevels'])
-        {
-            $todo['projectLevels'] = false;
-            $user->getProjectPerson()->set('todo',$todo);
-            $this->getEntityManager()->flush();
-
-            $plans = $user->getProjectPerson()->get('plans');
-            if (isset($plans['attend'])) $attend = strtolower($plans['attend']);
-            else                         $attend = null;
-            
-            if (strpos($attend,'yes') !== false)
-            {
-                return $this->redirect($this->generateUrl('_natgames_project_levels'));
-            }
-        }
         $tplData = $this->getTplData();
-        return $this->render('NatGamesBundle:Welcome:home.html.twig',$tplData);
+        return $this->render('Area5CFBundle:Welcome:home.html.twig',$tplData);
     }
 }
