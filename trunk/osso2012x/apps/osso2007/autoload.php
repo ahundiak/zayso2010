7f8fdef79d10a07f46729cb0f0a139bb2cb51259
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2011-08-16 21:30:14 +0000

    ah

---
+++ trunk/osso2012x/apps/osso2007/autoload.php
@@ -0,0 +1,50 @@
+<?php
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+
+$ws = '/home/ahundiak/zayso2012/';
+$ws = __DIR__ . '/../../../';
+
+$loader = new UniversalClassLoader();
+$loader->registerNamespaces(array(
+    'Symfony'          => array($ws.'Symfony/vendor/symfony/src', $ws.'Symfony/vendor/bundles'),
+    'Doctrine'         => $ws.'doctrine-orm',
+
+    'Sensio'           => $ws.'Symfony/vendor/bundles',
+    'JMS'              => $ws.'Symfony/vendor/bundles',
+
+    'Monolog'          => $ws.'Symfony/vendor/monolog/src',  // Some sort of logging package
+    'Assetic'          => $ws.'Symfony/vendor/assetic/src',  // Asset management (js, css etc)
+    'Metadata'         => $ws.'Symfony/vendor/metadata/src', // Not sure
+));
+$loader->registerPrefixes(array(
+    'Twig_Extensions_' => $ws.'Symfony/vendor/twig-extensions/lib',
+    'Twig_'            => $ws.'Symfony/vendor/twig/lib',
+));
+
+// intl
+if (!function_exists('intl_get_error_code')) {
+    require_once $ws.'Symfony/vendor/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+
+    $loader->registerPrefixFallbacks(array($ws.'Symfony/vendor/symfony/src/Symfony/Component/Locale/Resources/stubs'));
+}
+
+$loader->registerNamespaceFallbacks(array(
+    $ws.'osso2012x/src',
+));
+$loader->register();
+
+AnnotationRegistry::registerLoader(function($class) use ($loader) {
+    $loader->loadClass ($class);
+    return class_exists($class, false);
+});
+AnnotationRegistry::registerFile($ws.'doctrine-orm/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
+
+// Swiftmailer needs a special autoloader to allow
+// the lazy loading of the init file (which is expensive)
+require_once $ws.'Symfony/vendor/swiftmailer/lib/classes/Swift.php';
+Swift::registerAutoload($ws.'Symfony/vendor/swiftmailer/lib/swift_init.php');
+
+unset($ws);
