de3386e624b504dddda56987947b28e9c176ff86
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2011-09-09 20:11:53 +0000

    ah

--- trunk/osso2012x/src/Zayso/EaysoBundle/Service/EaysoManager.php
+++ trunk/osso2012x/src/Zayso/EaysoBundle/Service/EaysoManager.php
@@ -16,5 +16,27 @@ class EaysoManager
     {
         $this->em = $em;
     }
+    public function loadVolCerts($aysoid)
+    {
+        if (substr($aysoid,0,5) != 'AYSOV') $aysoid = 'AYSOV' . $aysoid;
+
+        // Build query
+        $em = $this->getEntityManager();
+        $qb = $em->createQueryBuilder();
+
+        $qb->addSelect('vol');
+        $qb->addSelect('cert');
+
+        $qb->from('EaysoBundle:Volunteer','vol');
+
+        $qb->leftJoin('vol.certifications','cert');
+
+        $qb->andWhere($qb->expr()->eq('vol.id',':aysoid'));
+        $qb->setParameter('aysoid',$aysoid);
+
+        $query = $qb->getQuery();
+        $item = $query->getSingleResult();
+        return $item;
+    }
 }
 ?>
