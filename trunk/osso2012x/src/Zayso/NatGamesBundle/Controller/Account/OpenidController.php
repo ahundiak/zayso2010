ca7315b8daef1829547e6fa5f643deeb23955a00
ahundiak <ahundiak@92133c96-8a7a-1287-154a-1d41c31ffe6d>
2011-11-23 17:19:16 +0000

    ah

---
+++ trunk/osso2012x/src/Zayso/NatGamesBundle/Controller/Account/OpenidController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Zayso\NatGamesBundle\Controller\Account;
+
+use Zayso\NatGamesBundle\Controller\BaseController;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Zayso\ZaysoBundle\Component\Security\User\OpenidProfile;
+
+class OpenidController extends BaseController
+{
+    public function addRpxAction(Request $request)
+    {
+        // Load the profile
+        $profile = $this->getOpenidprofile($request);
+        if (!is_array($profile))
+        {
+            $request->getSession()->setFlash('openid_add_error',$profile);
+            return $this->redirect($this->generateUrl('natgames_account_openid_add'));
+        }
+        // Maybe move all of this to account manager
+
+        // See if already have one
+        $accountManager = $this->getAccountManager();
+        $openid = $accountManager->getOpenidForIdentifier($profile['identifier']);
+        if ($openid) return $this->redirect($this->generateUrl('natgames_account_openid_add'));
+
+        // Make one
+        $em = $this->getAccountManager()->getEntityManager();
+        $openid = $accountManager->newOpenid($profile);
+
+        $user = $this->getUser();
+        $accountPersonId = $user->getAccountPersonId();
+        $accountPerson = $em->getReference('ZaysoBundle:AccountPerson',$accountPersonId);
+        $openid->setAccountPerson($accountPerson);
+
+        $em->persist($openid);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('natgames_account_openid_add'));
+    }
+    // Move to account manager?
+    protected function addOpenId($profile)
+    {
+        // Make sure identifier is not already in use
+        $accountManager = $this->getAccountManager();
+        $em = $this->getAccountManager()->getEntityManager();
+
+        $openid = $accountManager->getOpenidForIdentifier($profile->get('identifier'));
+        if ($openid) return;
+
+        $openid = $accountManager->newOpenid($profile);
+
+        $user = $this->getUser();
+        $accountPersonId = $user->getAccountPersonId();
+        $accountPerson = $em->getReference('ZaysoBundle:AccountPerson',$accountPersonId);
+
+        $openid->setAccountPerson($accountPerson);
+      //$openid->setPersonId       ($user->getPersonId());
+
+        $em->persist($openid);
+        $em->flush();
+    }
+    public function addAction(Request $request)
+    {
+        if ($request->getMethod() == 'POST') return $this->addPostAction($request);
+
+        $openids = $this->getAccountManager()->getOpenidsForAccount($this->getUser()->getAccountId());
+
+        $tplData = $this->getTplData();
+        $tplData['error'] = $request->getSession()->getFlash('openid_add_error');
+        $tplData['openids'] = $openids;
+        
+        return $this->render('NatGamesBundle:Account\Openid:add.html.twig',$tplData);
+
+        // New form stuff
+        $accountManager = $this->getAccountManager();
+        $accountPerson = $accountManager->newAccountPerson(array('projectId' => $this->getProjectId()));
+        $accountPerson->setAccountRelation('Family');
+        
+        $formType = $this->get('account.person.add.formtype');
+
+        $form = $this->createForm($formType, $accountPerson);
+
+        if ($request->getMethod() == 'POST')
+        {
+            $form->bindRequest($request);
+
+            if ($form->isValid())
+            {
+                $accountPerson = $this->addAccountPerson($accountPerson);
+                
+                //if ($accountPerson) return $this->redirect($this->generateUrl('_natgames_account_profile'));
+                
+            }
+            // else die('Not validated');
+        }
+        $tplData = $this->getTplData();
+        $tplData['form'] = $form->createView();
+
+        return $this->render('NatGamesBundle:Account\Person:add.html.twig',$tplData);
+    }
+    public function addPostAction(Request $request)
+    {
+        $info = $request->request->get('openids');
+        if (isset($info['remove']))
+        {
+            foreach($info['remove'] as $id)
+            {
+                $this->getAccountManager()->deleteOpenid($id);
+            }
+        }
+        return $this->redirect($this->generateUrl('natgames_account_openid_add'));
+
+    }
+}
